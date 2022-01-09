e6549c7ad4dc930bc6540d54454baee483749fa0
Thomas Becker <37275142+tjbecker8@users.noreply.github.com>
2022-01-09 22:50:28 +1300

    schedule ui working

---
+++ src/components/schedule/Game.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+const Game = (props) => {
+    return (
+        <div>
+
+          <Row>
+            <Col>{props.info.date}</Col>
+            <Col>{props.info.time}</Col>
+            <Col>{props.info.grade}</Col>
+            <Col>{props.info.ref1}</Col>
+            <Col>{props.info.ref2}</Col>
+            <Col>{props.info.ref3}</Col>
+          </Row>
+
+        </div>
+    )
+}
+
+export default Game
--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -3,11 +3,38 @@ import Stack from 'react-bootstrap/Stack'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Game from './Game.js'
+import Col from 'react-bootstrap/Col'
 
-const games = [1, 1, 1, 1, 1]
+
 
 
 const Schedule = () => {
+  const games = [
+    {
+      date: '12/12/21',
+      time: "18:30",
+      grade: "t1",
+      ref1: "tom",
+      ref2: "ted",
+      ref3: "rich",
+    },
+    {
+      date: '12/12/22',
+      time: "18:30",
+      grade: "t1",
+      ref1: "tom",
+      ref2: "ted",
+      ref3: "rich",
+    },
+    {
+      date: '12/12/23',
+      time: "18:30",
+      grade: "t1",
+      ref1: "tom",
+      ref2: "ted",
+      ref3: "rich",
+    }
+  ]
     return (
         <div>
         <Stack gap={3}>
@@ -20,15 +47,13 @@ const Schedule = () => {
               <Col>Ref/Line</Col>
               <Col>Line</Col>
             </Row>
-            {(games.length > 0 ) ? (
-        { games.map((i) => {
+
+        { games.map((i, index) => {
           return (
-         <Game key={i}/>
+         <Game info={i} key={index} />
         )})
       }
-      </div>
-      </div>
-    ) : <p>Games is not greater than 0 </p>}
+
         </Container>
         </Stack>
 

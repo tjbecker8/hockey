86dabce80b8b8acacd08f793e3a8e6093c12bdba
Thomas Becker <37275142+tjbecker8@users.noreply.github.com>
2022-01-13 02:08:36 +1300

    new game works, writes to firestore correctly

--- src/components/newGame/NewGame.js
+++ src/components/newGame/NewGame.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import './newGame.css'
@@ -7,34 +7,48 @@ import { collection, addDoc, setDoc, doc, } from "firebase/firestore";
 
 const NewGame = () => {
 
+  const [date, setDate] = useState(null);
+  const [grade, setGrade] = useState(null)
+
+
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("hey this shit works" );
-    const docRef =  await addDoc(collection(db, "cities"), {
-      name: "Los Angeles",
-      state: "CA",
-      country: "USA"
+    const docRef =  await addDoc(collection(db, "games"), {
+      dateTime: date,
+      grade: grade,
+
     });
     console.log("Document written with ID: ", docRef.id);
+    alert("Game Created")
   }
-  //now it is working, need to add hooks for info
+
+
+  const dateChange = (e) => {
+    e.preventDefault();
+    setDate(e.target.valueAsDate)
+  }
+
+  const gradeChange = (e) => {
+    e.preventDefault();
+    setGrade(e.target.value)
+  }
 
     return (
         <div className='newGameDiv'>
         <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3">
             <Form.Label>Date and Time</Form.Label>
-            <Form.Control type="datetime-local"  />
+            <Form.Control type="datetime-local" onChange={dateChange}  />
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label>Grade</Form.Label>
-            <Form.Select aria-label="Default select example">
-              <option>Open this select menu</option>
+            <Form.Select aria-label="Default select example" onChange={gradeChange} >
               <option value="1">G1</option>
               <option value="2">G2</option>
               <option value="3">G3</option>
               <option value="4">G4</option>
-              <option value="4">Other</option>
+              <option value="5">Other</option>
             </Form.Select>
           </Form.Group>
 

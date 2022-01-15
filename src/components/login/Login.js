1f9012aad2e9d3d8cdceea4e3fbf71dbac11af0f
Thomas Becker <37275142+tjbecker8@users.noreply.github.com>
2022-01-15 13:48:02 +1300

    Update Login.js

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,15 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import './Login.css'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 
 
 //need to add button for sending password reset link to email
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const loginAuth= () => {
+    const auth = getAuth();
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+    // Signed in
+      const user = userCredential.user;
+    // ...
+    })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  }
+
+  const passwordChange = (e) => {
+    e.preventDefault()
+    setPassword(e.target.value)
+
+  }
+
+  const emailChange =(e) => {
+    e.preventDefault()
+    setEmail(e.target.value)
+  }
+
+
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log("login button works");
   }
+
+
     return (
         <div className='loginDiv'>
         <h3>Login</h3>
@@ -17,12 +50,12 @@ const Login = () => {
         <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control type="email" placeholder="Enter email" onChange={emailChange} />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="password" placeholder="Password" onChange={passwordChange} />
           </Form.Group>
           <Button variant="primary" type="submit">
             Submit

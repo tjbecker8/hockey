ba31d1f9e248f94437978212e19ea1bcaf338311
Thomas Becker <37275142+tjbecker8@users.noreply.github.com>
2022-01-15 22:55:01 +1300

    setGames is not working

--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Stack from 'react-bootstrap/Stack'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Game from './Game.js'
 import Col from 'react-bootstrap/Col'
 import './Schedule.css';
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from '../../firebase';
+
 
 
 //change this to a table on react bootstrap
 
 const Schedule = () => {
-  const games = [
+  const [games, setGames] = useState([])
+  const [id, setid] = useState(1)
+
+  const getGames = async () => {
+    const today = new Date()
+    const yesterday = today.setDate(today.getDate() - 1)
+    const querySnapshot = await getDocs(collection(db, "games"), where("dateTime", ">=", yesterday));
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      setGames(games => [ ... games, {
+        date: doc.data().dateTime,
+        grade: doc.data().grade,
+        id: doc.id,
+        ref: doc.data().ref,
+        refLine: doc.data().refLine,
+        line: doc.data().line,
+        notes: doc.data().notes,
+      }])
+      console.log("info", doc.id, " => ", doc.data());
+});
+console.log("games", games);
+  }
+
+  useEffect(() => {
+    getGames()
+  }, [id]);
+
+  const gamesTest = [
     {
       date: '12/12/21',
       time: "18:30",
@@ -36,6 +66,9 @@ const Schedule = () => {
       ref3: "rich",
     }
   ]
+
+
+
     return (
         <div className='topDiv'>
         <Stack gap={3}>
@@ -49,7 +82,7 @@ const Schedule = () => {
               <Col>Line</Col>
             </Row>
 
-        { games.map((i, index) => {
+        { gamesTest.map((i, index) => {
           return (
          <Game info={i} key={index} />
         )})

4295ea2922c9f996841d15296c4ca06a4b821291
Thomas Becker <37275142+tjbecker8@users.noreply.github.com>
2022-01-15 21:25:27 +1300

    aad homepage work on routes

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -1,10 +1,9 @@
 import React from 'react'
-
+import './homepage.css'
 const HomePage = () => {
     return (
-        <div>
-        <br />
-        <br />
+        <div className="home">
+
         <h1>This is the home page</h1>
 
         </div>
--- src/components/topBar/TopBar.js
+++ src/components/topBar/TopBar.js
@@ -10,7 +10,7 @@ const TopBar = () => {
         <div>
           <Navbar bg="light" expand="lg" fixed="top">
             <Container>
-              <Navbar.Brand href="home">React-Bootstrap</Navbar.Brand>
+              <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">

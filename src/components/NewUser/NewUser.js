fa64a43eee8914b46c92e4f7215f5c1101f2a004
Thomas Becker <37275142+tjbecker8@users.noreply.github.com>
2022-01-17 16:13:16 +1300

    all working as it should

--- src/components/NewUser/NewUser.js
+++ src/components/NewUser/NewUser.js
@@ -3,12 +3,30 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import './newUser.css'
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { collection, addDoc, setDoc, doc, } from "firebase/firestore";
+import { db } from '../../firebase';
 
 
 const NewUser = () => {
 
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [userId, setUserId] = useState("")
+  const [displayName, setDisplayName] = useState('')
+  const [isRef, setIsRef] = useState(false)
+
+  const addUserDb = async (id) => {
+    await setDoc(doc(db, "users", id), {
+      displayName: displayName,
+      userId: id,
+      isRef: isRef,
+      image: "https://firebasestorage.googleapis.com/v0/b/refsched-7a9be.appspot.com/o/Screen%20Shot%202022-01-17%20at%2012.24.10%20PM.png?alt=media&token=52cfbbac-08ab-4d3e-86a2-387cc438f462",
+      phone: null,
+      facebook: null,
+      email: email,
+    });
+    console.log("Document written");
+  }
 
 const signUp = () => {
   const auth = getAuth();
@@ -16,8 +34,13 @@ const signUp = () => {
     .then((userCredential) => {
     // Signed in
     const user = userCredential.user;
+    const uid = user.uid
+    setUserId(uid)
+    addUserDb(uid)
+
     // ...
     console.log("user", user);
+    console.log("uid", uid);
     })
     .catch((error) => {
     const errorCode = error.code;
@@ -48,11 +71,31 @@ const signUp = () => {
     setEmail(e.target.value)
     console.log(e.target.value);
   }
+
+  const nameChange =(e) => {
+    e.preventDefault()
+    setDisplayName(e.target.value)
+    console.log(e.target.value);
+  }
+
+  const refChange = () => {
+    setIsRef(!isRef)
+  }
     return (
         <div className='newUserDiv'>
         <h3>Sign up for a new account</h3>
         <h5>If you already have an account. Sign in <a id="here">here</a></h5>
         <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Display Name</Form.Label>
+            <Form.Control type="text" placeholder="Display Name" onChange={nameChange} />
+              <Form.Text className="text-muted">
+                Your name to be displayed on the site and schedule
+              </Form.Text>
+          </Form.Group>
+
+
+
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter email" onChange={emailChange} />
@@ -63,9 +106,12 @@ const signUp = () => {
             <Form.Control type="password" placeholder="Password" onChange={passwordChange} />
             <Form.Text id="passwordHelpBlock" muted>
             Your password must be 8-20 characters long, and
-            must not contain spaces, special characters, or emoji.
+            must not contain spaces, special characters, or emoji. *need to do test for this, aand add alert
             </Form.Text>
           </Form.Group>
+          <input type="checkbox" value="Ref" name="Referee" checked={isRef} onChange={refChange} /> Referee?
+            <br></br>
+            <br></br>
           <Button variant="primary" type="submit">
             Submit
           </Button>
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -3,32 +3,57 @@ import Card from 'react-bootstrap/Card'
 import ListGroup from 'react-bootstrap/ListGroup'
 import ListGroupItem from 'react-bootstrap/ListGroupItem'
 import './profile.css'
-import { auth } from '../../firebase';
+import { auth, db } from '../../firebase';
 import { getAuth, updateProfile } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+
+
 
 const Profile = () => {
+
 
   const [name, setName] = useState('Tom Becker')
   const [image, setImage] = useState("https://firebasestorage.googleapis.com/v0/b/refsched-7a9be.appspot.com/o/Screen%20Shot%202022-01-17%20at%2012.24.10%20PM.png?alt=media&token=52cfbbac-08ab-4d3e-86a2-387cc438f462")
   const [phone, setPhone] = useState('555-5555')
   const [facebook, setFacebook] = useState('empty link')
   const [email, setEmail] = useState('me@here.com')
+  const [userId, setUserId] = useState("1")
+  const [isRef, setIsRef] = useState(false)
+
+  const auth = getAuth();
+
+
+
+
+  const getUserInfo =  async (id) => {
+    const docRef = doc(db, "users", id);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+      const data = docSnap.data()
+      setName(data.displayName)
+      setEmail(data.email)
+      setPhone(data.phone)
+      setIsRef(data.isRef)
+      setImage(data.image)
+      setFacebook(data.facebook)
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  }
 
 
 
   const profileUpdate = () => {
-    updateProfile(auth.currentUser, {
-      displayName: "Jane Q. User", photoURL: "https://example.com/jane-q-user/profile.jpg"
-      }).then(() => {
-        // Profile updated!
-        // ...
-        console.log("profile updated");
-      }).catch((error) => {
-        // An error occurred
-        // ...
-        console.log("err updateProfile");
-      });
+    
     }
+
+    useEffect(() => {
+      console.log("user", auth.currentUser.uid);
+      setUserId(auth.currentUser.uid)
+      getUserInfo(auth.currentUser.uid)
+    }, [userId]);
 
     return (
         <div className="profDiv">

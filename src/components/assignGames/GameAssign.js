003639984568553808af97764816f75d899b981b
Thomas Becker <37275142+tjbecker8@users.noreply.github.com>
2022-01-18 21:27:03 +1300

    trying to finishing setting up game assignments

--- src/components/assignGames/GameAssign.js
+++ src/components/assignGames/GameAssign.js
@@ -1,26 +1,51 @@
 import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button'
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db, auth, } from '../../firebase';
+import GameAssignVerify from './GameAssignVerify'
 
 const GameAssign = (props) => {
 
   const [date, setDate] = useState('')
   const [id, setId] = useState('')
   const [assigned, setAssigned] = useState(false)
+  const [requests, setRequests] = useState([])
+
+  const length = requests.length
 
 
   const Assign = () => {
     setAssigned(!assigned)
   }
 
+  const getRequests = async (i) => {
+    const q = query(collection(db, "games", i, "requested"), where("requested", "==", true));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      setRequests(requests => [... requests, {
+        data: doc.data(),
+        id: doc.id,
+        name: doc.data().name
+      }])
+
+      console.log(doc.id, " => ", doc.data());
+    });
+
+  }
+
   useEffect(() => {
-    console.log("mm", props.info.data);
+    console.log("mm", requests.length);
     setId(props.info.id)
     let thisDate = new Date(props.info.data.dateTime.seconds * 1000)
     let localDate = thisDate.toLocaleString()
     setDate(localDate)
+    getRequests(props.info.id)
     }, []);
 
     return (
+
+
 
           <tr>
             <td>{date}</td>
@@ -35,7 +60,9 @@ const GameAssign = (props) => {
 
           </tr>
 
-    )
-}
+        )
+  }
+
+
 
 export default GameAssign

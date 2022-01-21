befc98801f66f227cd19cd10b52c4a71a30b2922
Thomas Becker <37275142+tjbecker8@users.noreply.github.com>
2022-01-21 19:08:24 +1300

    updating my games

--- src/components/myGames/Game.js
+++ src/components/myGames/Game.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, } from 'react'
 import Button from 'react-bootstrap/Button'
-import { doc, updateDoc } from "firebase/firestore";
+import { doc, updateDoc, getDocs, collection, where } from "firebase/firestore";
 import { db } from '../../firebase';
 
 
@@ -11,16 +11,29 @@ const [id, setId] = useState('')
 const [date, setDate] = useState('')
 const [confirmed, setConfirmed] = useState(false)
 const [uid, setUid] = useState('')
+const [refs, setRefs] = useState([])
+
+const getRefs = async (gameId) => {
+  const gamesSnapshot = await getDocs(collection(db, "games", gameId, "requested"), where("assigned", "==", true));
+  gamesSnapshot.forEach((doc) => {
+    console.log("other refs", doc.data(), doc.id);
+    setRefs(refs => [ ... refs, {
+      data: doc.data(),
+      id: doc.id
+    }])
+  })
+}
 
 
   useEffect(() => {
-
     setId(props.info.data.game)
+
     setConfirmed(props.info.data.accepted)
     let thisDate = new Date(props.info.data.date.seconds * 1000)
     let localDate = thisDate.toLocaleString()
     setDate(localDate)
     setUid(props.uid)
+    getRefs(props.info.data.game)
   }, [])
 
   const AcceptGame = () => {
@@ -42,17 +55,19 @@ const [uid, setUid] = useState('')
       accepted: true
     });
     //need function to set the ref, ref/line, or line slot
-
-
   }
+
+
 
     return (
       <tr>
         <td>{date}</td>
         <td>Grade</td>
-        <td>Ref</td>
-        <td>ref/Line</td>
-        <td>Line</td>
+        {(refs[0] ? <td>{refs[0].data.name}</td> : <td>no ref</td>)}
+        {(refs[1] ? <td>{refs[1].data.name}</td> : <td>no ref</td>)}
+        {(refs[2] ? <td>{refs[2].data.name}</td> : <td>no ref</td>)}
+
+
         <td>{(!confirmed ? <Button variant="warning" onClick={() => {AcceptGame()}}>Accept Game</Button> : <Button variant="success" onClick={() => {AcceptGame()}}>Game Accepted</Button>)}</td>
       </tr>
     )
--- src/components/myGames/MyGames.js
+++ src/components/myGames/MyGames.js
@@ -1,47 +1,54 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import './myGames.css'
 import Table from 'react-bootstrap/Table'
 import { auth, db, user } from '../../firebase';
 import { getAuth, updateProfile, onAuthStateChanged } from "firebase/auth";
 import { doc, getDoc, where, getDocs, collection, } from "firebase/firestore";
 import Game from './Game'
+import { AuthContext } from "../../auth";
 
+//need option to reject games
 
 const MyGames = () => {
   const [userId, setUserId] = useState('')
-  const [gameIds, setGameIds] =useState([])
+  const [gameIds, setGameIds] = useState([])
   const [games, setGames] = useState([])
+  const [refs, setRefs] =useState([])
+  const { currentUser, loading } = useContext(AuthContext);
+
 
   const getMyGames = async (id) => {
     const today = new Date()
     const yesterday = today.setDate(today.getDate() - 1)
     const querySnapshot = await getDocs(collection(db, "users", id, "games"), where("date", ">=", yesterday));
 
-    querySnapshot.forEach((doc) => {
-      console.log(doc.data());
+    querySnapshot.forEach( async (doc) => {
+      // console.log("working?", doc.data());
       setGames(games => [ ... games, {
         data: doc.data(),
         id: doc.id,
+        refs: refs,
       }])
     })
   }
 
-  const getUserId = async () => {
-  await onAuthStateChanged(auth, (user) => {
-    if (user) {
-      const uid = user.uid;
-      // console.log("user", uid);
-      setUserId(uid)
-      getMyGames(uid)
-
-    } else {
-    console.log("no user");
-    }
-  });
-  }
+  // const getUserId = async () => {
+  // await onAuthStateChanged(auth, (user) => {
+  //   if (user) {
+  //     const uid = user.uid;
+  //     // console.log("user", uid);
+  //     setUserId(uid)
+  //     getMyGames(uid)
+  //
+  //   } else {
+  //   console.log("no user");
+  //   }
+  // });
+  // }
 
   useEffect(() => {
-    getUserId()
+    setUserId(currentUser.uid)
+    getMyGames(currentUser.uid)
     }, []);
 
     return (
